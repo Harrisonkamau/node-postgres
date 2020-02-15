76dcc5f35f8c6d89b0554a0dff84a238de515127
Harrisonkamau <kamauharrison87@gmail.com>
2020-02-15 13:51:26 +0300

    Install loggery

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,22 @@
 const router = require('koa-joi-router');
 const homeRoute = require('./home');
 const userRoutes = require('./users');
+const statusRoute = require('./status');
 
 const homeRouter = router();
 homeRouter.prefix('');
 homeRouter.route(homeRoute);
 
+const statusRouter = router();
+statusRouter.prefix('/status');
+statusRouter.route(statusRoute);
+
 const userRouter = router();
-userRouter.prefix('/users/');
+userRouter.prefix('/users');
 userRouter.route(userRoutes);
 
 module.exports = [
   homeRouter,
   userRouter,
+  statusRouter,
 ];
